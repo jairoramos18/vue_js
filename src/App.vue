9b2e565fea73ba40b05628522ceefe669f2739ab
Camilo Galan <cagalano@ufpso.edu.co>
2023-06-05 12:05:07 -0500

    avances

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import CardComponent from '@/components/cardComponent.vue';
+
+const users = [ 
+   {
+      name : 'juana de arcos',
+      mail : 'juana@gmail.com',
+   },
+
+   {
+      name : 'pablo de arcos',
+      mail : 'pablo@gmail.com',
+   }
+]
+
+</script>
+
+<template>
+ 
+<h1> 
+   hola mundo
+</h1>
+
+<CardComponent  v-for="user int users"  :key="usuario"/>
+
+
+</template>
+
+<style >
+
+
+</style>
---
+++ src/components/cardComponent.vue
@@ -0,0 +1,18 @@
+<script  setup >
+const mensaje = () => {
+ console.log("Mensaje");
+}
+</script>
+
+<template>
+    <div class="w-48 rounded shadow overflow-hidden">
+   <div class=" inline-block  align-top ">
+      <img src="https://img.freepik.com/foto-gratis/disparo-gran-angular-solo-arbol-que-crece-cielo-nublado-puesta-sol-rodeada-cesped_181624-22807.jpg" alt="">
+   </div>
+   <div class=" inline-block">
+      <h2 class="text-md font-semibold"> pepito perez</h2>
+      <p class=" text-xs leading-0 ">correo@mipagina.com</p>
+   </div>
+</div>
+
+</template>
